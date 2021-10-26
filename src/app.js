49f5fc56793ec4a518c998b7859ceba20d1f0156
ismlzdmr <isml4470@outlook.com>
2021-10-27 00:52:04 +0300

    s

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 console.log("Merhaba Kodlama.io")
+// javascript typesafe değildir
+let dolarDun = 9.2
+let dolarBugun = 9.3
+
+{
+    let dolarDun = 9.1
+    console.log(dolarDun)
+}
+
+console.log(dolarDun)
+// const sadece okunabilir 
+const euroDun = 11
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu-komut Kredisi"]
+
+console.log("<ul>")
+
+for (let i = 0; i<3; i++ ) {
+    console.log("<li>" + konutKredileri[i] + "<li>" )
+}
+console.log("<ul>")
+
